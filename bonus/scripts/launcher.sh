363e2db49d8271faa18b430cecc265c42d7c677a
zenon0777 <abderrahmane.daifi@protonmail.com>
2024-11-08 20:36:09 +0100

    inception of things ready to push

--- bonus/scripts/launcher.sh
+++ bonus/scripts/launcher.sh
@@ -4,5 +4,5 @@ sed -i 's|https://github.com/zenon0777/K3d-and-Argo-CD-adaifi.git|https://159.20
 kubectl config set-context --current --namespace=argocd
 argocd repo add http://159.203.187.180:8181/root/iot.git \
   --username root \
-  --password glpat-3uoo1gb-C8DCM873dVrq
+  --password $ARGOCD_PASSWORD_TOKEN
 kubectl apply -f iot/application.yaml
