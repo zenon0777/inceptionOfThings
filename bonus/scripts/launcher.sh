7fc41a226cace3ed51bdc54bac5755aa02406361
zenon0777 <abderrahmane.daifi@protonmail.com>
2024-11-08 20:38:41 +0100

    gitlab token for argocd

--- bonus/scripts/launcher.sh
+++ bonus/scripts/launcher.sh
@@ -1,8 +1,9 @@
+ARGOCD_PASSWORD_TOKEN=""
 git clone http://159.203.187.180:8181/root/iot.git
 cp -r  K3d-and-Argo-CD-adaifi/* iot/
 sed -i 's|https://github.com/zenon0777/K3d-and-Argo-CD-adaifi.git|https://159.203.187.180:8181/root/iot.git|g' iot/application.yaml
 kubectl config set-context --current --namespace=argocd
 argocd repo add http://159.203.187.180:8181/root/iot.git \
   --username root \
-  --password
+  --password $ARGOCD_PASSWORD_TOKEN
 kubectl apply -f iot/application.yaml
